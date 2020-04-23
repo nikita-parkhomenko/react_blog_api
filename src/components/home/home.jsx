23ff952220c5471d54e1880cae423e1fb57ba308
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-04-23 22:09:49 +0300

    Using axios library fetching posts data from API and storing in Home component state.

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Home = () => {
-    return(
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p> Deserunt duis aliquip excepteur commodo magna aliquip. </p>
-        </div>
-    )
+class Home extends Component {
+
+    state = {
+        posts: [ ]
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(resp => {
+                console.log(resp)
+                this.setState({
+                    posts: resp.data.slice(0, 10)
+                })
+            })
+    }
+
+    render() {
+        const { posts } = this.state;
+
+        const postList = posts.length ? (
+            posts.map(post => {
+                return(
+                    <div key={post.id} className="post card">
+                        <div className="card-content">
+                            <span className="card-title"> {post.title} </span>
+                            <p> {post.body} </p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center"> No Posts Yet! </div>
+        )
+        return(
+            <div className="container">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
+        )
+    }
 };
 
 export default Home;
