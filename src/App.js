16bc63e99d7d970ed8171a92c04246958537ff8e
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-04-24 18:40:18 +0300

    Implemented react-router-dom library for navigaition. Created Post component for fetching and rendering full post

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 
-import Navbar from './components/navbar/navbar';
-import Home from './components/home/home';
-import About from './components/about/about';
-import Contact from './components/contact/contact';
+import Navbar from './components/navbar/navbar'
+import Home from './components/home/home'
+import About from './components/about/about'
+import Contact from './components/contact/contact'
+import Post from './components/post/post'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/:post_id" component={Post} />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
